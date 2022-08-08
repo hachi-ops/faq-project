772aa7c2bf980e5ff8e1b107f60d72d07c10daa1
hachi-ops <g.helen.rog@gmail.com>
2022-08-08 02:00:44 +0100

    ListQuestions component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import AddQuestion from "./components/AddQuestion";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+// import Home from "./pages/Home";
+import ListQuestions from "./components/ListQuestions";
+// import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Home />
-      <Login />
+      {/* <Login /> */}
       <AddQuestion />
+      {/* <Home /> */}
+      <ListQuestions />
     </>
   );
 }
