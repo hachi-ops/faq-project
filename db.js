33f32be9e7b991a6b38acfb141592d04636801eb
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-08-22 20:56:29 +0100

    uniform local and remote set up for database

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
+const { Console } = require("console");
+
 const Pool = require("pg").Pool;
 require("dotenv").config();
-
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-};
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL, //heroku addons
   ssl: { rejectUnauthorized: false },
 };
 
+const devConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+  // ssl: { rejectUnauthorized: false },
+};
+
+console.log(devConfig);
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
