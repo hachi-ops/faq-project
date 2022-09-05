ff2a76e1ec3455769e0e4701e5fc03c16e14eba0
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-09-05 20:18:38 +0100

    database changes

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 -- CREATE DATABASE faq_database;
 
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
 CREATE TABLE questions(
-  id BIGSERIAL PRIMARY KEY,
-  question VARCHAR(300),
-  answer VARCHAR(500)
+  id SERIAL,
+  user_id UUID,
+  question VARCHAR(300) NOT NULL,
+  answer VARCHAR(500),
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
